e052cb3c0c0277331ce2da17346090dbf39b6df2
unknown <bridgetott@yahoo.com>
2020-09-21 11:41:41 -0500

    initial

---
+++ RPSLS/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    abstract class Player
+    {
+        //member Variable (HAS A)
+
+
+        //Constoctor (INITIAL VALUEE)
+
+
+        //Member Method (CAN DO)
+
+    }
+}
